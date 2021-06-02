bf8d2148f21783712838ecb39395ba88bbef9aa0
ninobrown585 <adrian.brown1@g.austincc.edu>
2021-06-01 20:43:32 -0500

    created models, packages, server,public, and readme

---
+++ models/workout.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    excercises: [
+        {
+            type: {
+                type: String,
+                required: "Please enter an exercise type"
+            },
+            name: {
+                type: String,
+                required: "Please enter name"
+            },
+            duration: {
+                type: Number,
+                required: "Please enter duration"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
